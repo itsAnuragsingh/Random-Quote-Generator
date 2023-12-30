39e7db2bf9d64371e962d8031737e0868d83e287
Anurag Singh <143262627+itsAnuragsingh@users.noreply.github.com>
2023-12-30 17:31:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,56 @@
 const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector(".newQ"),
-authorName = document.querySelector(".name"),
-speechBtn = document.querySelector(".speech"),
-copyBtn = document.querySelector(".copy"),
-twitterBtn = document.querySelector(".twitter"),
-synth = speechSynthesis;
-copyBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(quoteText.innerText);
+  quoteBtn = document.querySelector(".newQ"),
+  authorName = document.querySelector(".name"),
+  speechBtn = document.querySelector(".speech"),
+  copyBtn = document.querySelector(".copy"),
+  twitterBtn = document.querySelector(".twitter"),
+  synth = window.speechSynthesis;
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
 });
 
-twitterBtn.addEventListener("click", ()=>{
-    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
-    window.open(tweetUrl, "_blank");
+twitterBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetUrl, "_blank");
 });
 
-
-
-function randomQuote(){
-    quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
-    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
     });
 }
 
-speechBtn.addEventListener("click", ()=>{
-    if(!quoteBtn.classList.contains("loading")){
-        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
-        synth.speak(utterance);
-        etInterval(()=>{
-            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
-        }, 10);
-    }
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(
+      `${quoteText.innerText} by ${authorName.innerText}`
+    );
+    synth.speak(utterance);
+    setInterval(() => {
+      !synth.speaking
+        ? speechBtn.classList.remove("active")
+        : speechBtn.classList.add("active");
+    }, 10);
+  }
 });
 
-quoteBtn.addEventListener("click", randomQuote)
-
-
-
+quoteBtn.addEventListener("click", randomQuote);
 
 const overlay = document.querySelector(".overlay"),
-closeBtn = document.querySelector(".close-btn");
+  closeBtn = document.querySelector(".close-btn");
 
 copyBtn.addEventListener("click", () => document.body.classList.add("active"));
-
 overlay.addEventListener("click", () =>
-document.body.classList.remove("active")
+  document.body.classList.remove("active")
 );
-
 closeBtn.addEventListener("click", () =>
-document.body.classList.remove("active")
+  document.body.classList.remove("active")
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
